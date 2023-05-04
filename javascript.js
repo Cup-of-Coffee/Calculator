ac11f90e34fc921ee4efa05cbaca81d25f3ca83d
Cup-of-Coffee <larry.le124@gmail.com>
2023-05-04 15:47:41 -0400

    operate Function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,31 @@
 
+let numberFirst;
+let operator;
+let numberSecond;
+
+
+
+function display (){
+
+};
+
+
+
+function operate (valueOne, valueTwo, operator){
+    if(operator === 'addition'){
+        return;
+    }else if(operator === 'subtraction'){
+        return;
+    }else if(operator === 'multiplication'){
+        return;
+    }else if(operator === 'division'){
+        return;
+    }else{
+        return;
+    };
+};
+
+
 
 
 
